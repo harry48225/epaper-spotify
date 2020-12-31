a9c310cc6996eaa8bcf1a596eff720fbce74a2b2
Harry <harrybest800@gmail.com>
2020-12-31 20:33:33 +0000

    Added text to image

--- ImageCreator.py
+++ ImageCreator.py
@@ -1,14 +1,18 @@
-from PIL import Image, ImageOps, ImageDraw
+from PIL import Image, ImageOps, ImageDraw, ImageFont
+
+import textwrap
 import spotify
 import requests
 from io import BytesIO
 url = "https://i.scdn.co/image/ab67616d00001e02792675f3474149244b0a65a2"
 
 class ImageCreator(object):
-
+    
+    
     def __init__(self):
+        self.FONT_SIZE = 10
         self.sp = spotify.Spotify_client()
-
+        self.font = ImageFont.truetype("font/PressStart2P-Regular.ttf", self.FONT_SIZE)
 
     def get_album_art(self, url):
         
@@ -31,7 +35,29 @@ class ImageCreator(object):
 
         canvas.paste(grey_album_art)
 
+        draw = ImageDraw.Draw(canvas)
+
+        # Time to draw the artist, album, and track_name
+        y = 16
+
+        y = self.draw_wrapped_text(draw, song.get_artist() + ",", self.font, (136,y), self.FONT_SIZE)
+        y = self.draw_wrapped_text(draw, "|", self.font, (136,y), self.FONT_SIZE)
+        y = self.draw_wrapped_text(draw, song.get_album_name() + ",", self.font, (136,y), self.FONT_SIZE)
+        y = self.draw_wrapped_text(draw, "|", self.font, (136,y), self.FONT_SIZE)
+        y = self.draw_wrapped_text(draw, song.get_track_name(), self.font, (136,y), self.FONT_SIZE)
+
         canvas.show()
+
+    def draw_wrapped_text(self, draw, text, font, position, size):
+        # Return the y coordinate of the last line of text
+
+        x = position[0]
+        y = position[1]
+        for line in textwrap.wrap(text, width=int(128/self.FONT_SIZE)):
+            draw.text((x,y), line, font = font, fill = 0)
+            y += (size+2)
+
+        return y
 
 
     
--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 import spotify
+import ImageCreator
 
 sp = spotify.Spotify_client()
 
 song = (sp.get_current_song())
 
 print("track: {}, artist: {}, album: {}, art: {}".format(song.get_track_name(), song.get_artist() ,song.get_album_name(), song.get_album_art_url()))
+
+
+i = ImageCreator.ImageCreator()
+
+i.get_image()
