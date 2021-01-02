ecf0785f36864c835e4152651e882f9614d887e9
Harry <harrybest800@gmail.com>
2021-01-02 20:09:34 +0000

    improved text wrapping

--- ImageCreator.py
+++ ImageCreator.py
@@ -11,6 +11,8 @@ class ImageCreator(object):
     
     def __init__(self):
         self.FONT_SIZE = 10
+        self.SCREEN_WIDTH = 250
+        self.SCREEN_HEIGHT = 122
         
         self.font = ImageFont.truetype("font/PressStart2P-Regular.ttf", self.FONT_SIZE)
 
@@ -28,29 +30,58 @@ class ImageCreator(object):
     def get_image(self, song):
         grey_album_art = self.get_album_art(song.get_album_art_url())
 
-        canvas = Image.new('1', (250, 122), 255) # Empty image of the correct size
+        canvas = Image.new('1', (self.SCREEN_WIDTH, self.SCREEN_HEIGHT), 255) # Empty image of the correct size
 
         canvas.paste(grey_album_art)
 
         draw = ImageDraw.Draw(canvas)
 
         # Time to draw the artist, album, and track_name
-        y = 16
+        
+        # VERTICAL CENTERING DONE - MAYBE HORIZONTAL?
+        text_to_draw = [song.get_artist()+",", "|", song.get_album_name() + ",", "|", song.get_track_name()]
 
-        y = self.draw_wrapped_text(draw, song.get_artist() + ",", self.font, (136,y), self.FONT_SIZE)
-        y = self.draw_wrapped_text(draw, "|", self.font, (136,y), self.FONT_SIZE)
-        y = self.draw_wrapped_text(draw, song.get_album_name() + ",", self.font, (136,y), self.FONT_SIZE)
-        y = self.draw_wrapped_text(draw, "|", self.font, (136,y), self.FONT_SIZE)
-        y = self.draw_wrapped_text(draw, song.get_track_name(), self.font, (136,y), self.FONT_SIZE)
+        text_start = 136
+        height = self.get_multi_text_height(text_start, text_to_draw)
 
+        print(height)
+
+        y = max(int((self.SCREEN_HEIGHT - height) / 2), 0)
+        for text in text_to_draw:
+            y = self.draw_wrapped_text(draw, text, self.font, (text_start,y), self.FONT_SIZE)
+        
+        print(y)
         return canvas
+
+    def get_multi_text_height(self,text_start, text_to_draw):
+        # 'Dry run' of the image drawing
+        canvas = Image.new('1', (self.SCREEN_WIDTH, self.SCREEN_HEIGHT), 255) # Empty image of the correct size
+
+        draw = ImageDraw.Draw(canvas)
+
+        y = 0
+
+        for text in text_to_draw:
+            y = self.draw_wrapped_text(draw, text, self.font, (text_start,y), self.FONT_SIZE)
+
+        return y
+
 
     def draw_wrapped_text(self, draw, text, font, position, size):
         # Return the y coordinate of the last line of text
 
         x = position[0]
         y = position[1]
-        for line in textwrap.wrap(text, width=int(100/self.FONT_SIZE)):
+
+        # Font appears to be monospace so the wrapping is easier
+        # display is 250px wide,
+        # So we have (250px - x) px to fit the text
+        for line in textwrap.wrap(text, width=int((self.SCREEN_WIDTH - x)/self.FONT_SIZE)):
+
+            # If the comma is going to be on its own line, skip it
+            if line.strip() == ",":
+                continue
+
             draw.text((x,y), line, font = font, fill = 0)
             y += (size+2)
 
--- test.py
+++ test.py
@@ -1,8 +1,17 @@
 import spotify
-import ImageCreator
+from PIL import Image, ImageDraw, ImageFont
+from ImageCreator import ImageCreator
 
 sp = spotify.Spotify_client()
 
-song = (sp.get_current_song())
 
-print("track: {}, artist: {}, album: {}, art: {}".format(song.get_track_name(), song.get_artist() ,song.get_album_name(), song.get_album_art_url()))
+i = ImageCreator()
+
+while True:
+    song = (sp.get_current_song())
+
+    print("track: {}, artist: {}, album: {}, art: {}".format(song.get_track_name(), song.get_artist() ,song.get_album_name(), song.get_album_art_url()))
+
+    i.get_image(song).show()
+
+    input()
