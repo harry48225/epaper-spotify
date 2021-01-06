bf2b06edcebdbe0345b4e5bf6c3aff8eff9a99b7
Harry <harrybest800@gmail.com>
2021-01-06 20:52:30 +0000

    Moved to APScheduler based approach

---
+++ App.py
@@ -0,0 +1,45 @@
+from waveshare_epd import epd2in13_V2
+import spotify
+import time
+from ImageCreator import ImageCreator
+
+
+class App(object):
+
+    def __init__(self):
+        
+        print("start")
+        self.epd = epd2in13_V2.EPD()
+        print("display init")
+        self.epd.init(self.epd.FULL_UPDATE)
+        self.epd.Clear(0xFF)
+
+
+        self.i = ImageCreator()
+        self.sp = spotify.Spotify_client()
+        self.last_song = None
+
+    def update(self):
+
+        current_song = self.sp.get_current_song()
+
+        # If there is a currently playing song
+        if current_song:
+
+            # Only update if the song is new
+            if not (self.last_song and current_song.get_track_name() == self.last_song.get_track_name()):
+
+                # Display the image
+                print("getting image")
+                self.epd.display(self.epd.getbuffer(self.i.get_image(current_song)))
+
+        else:
+
+            # Display no song message if the last song wasn't none,
+            if self.last_song:
+                print("no song")
+                # Create a no song playing message
+                self.epd.display(self.epd.getbuffer(self.i.get_no_song_playing_image()))
+
+
+        self.last_song = current_song
--- main.py
+++ main.py
@@ -1,43 +1,11 @@
-from waveshare_epd import epd2in13_V2
-import spotify
-import time
-from ImageCreator import ImageCreator
+from App import App
 
 from apscheduler.schedulers.background import BackgroundScheduler
 
-print("start")
-epd = epd2in13_V2.EPD()
-print("display init")
-epd.init(epd.FULL_UPDATE)
-epd.Clear(0xFF)
+app = App()
 
-i = ImageCreator()
-sp = spotify.Spotify_client()
-last_song = None
+scheduler = BackgroundScheduler(coalesce = True)
+scheduler.start() # Should perform our update jobs in the background
+scheduler.add_job(app.update, 'interval', seconds=2)
 
-while True:
-    current_song = sp.get_current_song()
-
-    # If there is a currently playing song
-    if current_song:
-
-        # Only update if the song is new
-        if not (last_song and current_song.get_track_name() == last_song.get_track_name()):
-
-            # Display the image
-            print("getting image")
-            epd.display(epd.getbuffer(i.get_image(current_song)))
-
-    else:
-
-        # Display no song message if the last song wasn't none,
-        if last_song:
-            print("no song")
-            # Create a no song playing message
-            epd.display(epd.getbuffer(i.get_no_song_playing_image()))
-
-
-    last_song = current_song
-    time.sleep(2) # Sleep for 2 seconds
-
-
+input("Press enter to quit") # Stop the application from shutting down
